f629d1755a64fc31cf5fff78fef0ed4dc6243c79
Z-100 <marvin131t@gmail.com>
2025-06-04 13:24:49 +0200

    mesh

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ void process_inputs(GLFWwindow* window);
 unsigned int compile_glsl_shader(const char* shaderName, unsigned int shaderType);
 unsigned int build_shader_program();
 
-void bind_vao_vbo(unsigned int* vertexBuffer, unsigned int* vertexArray);
+void bind_vbo_vao_ebo(unsigned int* vertexBuffer, unsigned int* vertexArray, unsigned int* elementBuffer);
 
 int main() {
 
@@ -43,10 +43,15 @@ int main() {
 	}
 
 	unsigned int shaderProgram = build_shader_program();
-	unsigned int vertexBuffer, vertexArray;
-	bind_vao_vbo(&vertexBuffer, &vertexArray);
 
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	unsigned int vertexBuffer, // VBO
+		vertexArray,		   // VAO
+		elementBuffer		   // EBO
+		;
+
+	bind_vbo_vao_ebo(&vertexBuffer, &vertexArray, &elementBuffer);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	while (!glfwWindowShouldClose(mainWindow)) {
 
 		process_inputs(mainWindow);
@@ -54,9 +59,11 @@ int main() {
 		glClearColor(screenColors[0], screenColors[1], screenColors[2], 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Triangle magic
 		glUseProgram(shaderProgram);
+
 		glBindVertexArray(vertexArray);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(mainWindow);
@@ -116,24 +123,38 @@ unsigned int build_shader_program() {
 	return shaderProgram;
 }
 
-void bind_vao_vbo(unsigned int* vertexBuffer, unsigned int* vertexArray) {
+void bind_vbo_vao_ebo(unsigned int* vertexBuffer, unsigned int* vertexArray, unsigned int* elementBuffer) {
 
 	// clang-format off
 	float vertices[] = {
-		-0.5f, -0.5f, 0,
-		 0.5f, -0.5f, 0,
-		    0,  0.5f, 0,
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+	   -0.5f, -0.5f, 0.0f,  // bottom left
+	   -0.5f,  0.5f, 0.0f   // top left
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		// 1, 2, 3,
+		3,2,1
 	};
 	// clang-format on
 
 	glGenVertexArrays(1, vertexArray);
 	glGenBuffers(1, vertexBuffer);
+	glGenBuffers(1, elementBuffer);
 
 	glBindVertexArray(*vertexArray);
 
+	// Bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, *vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Bind EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *elementBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Define vertices structure
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 	glEnableVertexAttribArray(0);
 
