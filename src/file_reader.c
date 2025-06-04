3992be78fa7b44681fdc421e8750359b0d915e57
Z-100 <marvin131t@gmail.com>
2025-06-04 13:06:19 +0200

    formatter

--- src/file_reader.c
+++ src/file_reader.c
@@ -1,32 +1,32 @@
 #include "file_reader.h"
 
-#include <stdio.h>
 #include <glad/glad.h>
+#include <stdio.h>
 
 char* readLines(const char* filePath) {
 
-    FILE* filePtr = fopen(filePath, "r");
+	FILE* filePtr = fopen(filePath, "r");
 
-    if (!filePtr) {
-        perror(strcat("File not found: ", filePath));
-        return nullptr;
-    }
+	if (!filePtr) {
+		perror(strcat("File not found: ", filePath));
+		return nullptr;
+	}
 
-    fseek(filePtr, 0, SEEK_END);
-    const int fileLength = ftell(filePtr);
-    rewind(filePtr);
+	fseek(filePtr, 0, SEEK_END);
+	const int fileLength = ftell(filePtr);
+	rewind(filePtr);
 
-    char* fileContentBuffer = calloc(fileLength + 1, sizeof(char));
+	char* fileContentBuffer = calloc(fileLength + 1, sizeof(char));
 
-    if (!fileContentBuffer) {
-        perror(strcat("Couldn't calloc memory for: ", filePath));
-        return nullptr;
-    }
+	if (!fileContentBuffer) {
+		perror(strcat("Couldn't calloc memory for: ", filePath));
+		return nullptr;
+	}
 
-    const size_t bytesRead = fread(fileContentBuffer, 1, fileLength, filePtr);
-    fileContentBuffer[fileLength > bytesRead ? bytesRead : fileLength] = '\0';
+	const size_t bytesRead = fread(fileContentBuffer, 1, fileLength, filePtr);
+	fileContentBuffer[fileLength > bytesRead ? bytesRead : fileLength] = '\0';
 
-    fclose(filePtr);
+	fclose(filePtr);
 
-    return fileContentBuffer;
+	return fileContentBuffer;
 }
--- src/main.c
+++ src/main.c
@@ -1,161 +1,165 @@
-#include <stdio.h>
-#include <string.h>
-#include <wchar.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 #include "file_reader.h"
 
-const char *TITLE = "y5";
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
-float screenColors[] = {0, 1, 0};
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+const char* TITLE  = "y5";
+const int	WIDTH  = 800;
+const int	HEIGHT = 600;
 
-void process_inputs(GLFWwindow *window);
+float screenColors[] = { 0, 1, 0 };
 
-unsigned int compile_glsl_shader(const char *shaderName, unsigned int shaderType);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_inputs(GLFWwindow* window);
 
+unsigned int compile_glsl_shader(const char* shaderName, unsigned int shaderType);
 unsigned int build_shader_program();
 
-void bind_vao_vbo(unsigned int *vertexBuffer, unsigned int *vertexArray);
+void bind_vao_vbo(unsigned int* vertexBuffer, unsigned int* vertexArray);
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (mainWindow == nullptr) {
-        printf("Error creating window");
-        glfwTerminate();
-        return -1;
-    }
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
 
-    glfwMakeContextCurrent(mainWindow);
-    glfwSetFramebufferSizeCallback(mainWindow, framebuffer_size_callback);
+	if (mainWindow == nullptr) {
+		printf("Error creating window");
+		glfwTerminate();
+		return -1;
+	}
 
-    if (!gladLoadGLLoader(glfwGetProcAddress)) {
-        printf("Error loading GLAD");
-        return -1;
-    }
+	glfwMakeContextCurrent(mainWindow);
+	glfwSetFramebufferSizeCallback(mainWindow, framebuffer_size_callback);
 
-    unsigned int shaderProgram = build_shader_program();
-    unsigned int vertexBuffer, vertexArray;
-    bind_vao_vbo(&vertexBuffer, &vertexArray);
+	if (!gladLoadGLLoader(glfwGetProcAddress)) {
+		printf("Error loading GLAD");
+		return -1;
+	}
 
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    while (!glfwWindowShouldClose(mainWindow)) {
-        process_inputs(mainWindow);
+	unsigned int shaderProgram = build_shader_program();
+	unsigned int vertexBuffer, vertexArray;
+	bind_vao_vbo(&vertexBuffer, &vertexArray);
 
-        glClearColor(screenColors[0], screenColors[1], screenColors[2], 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	while (!glfwWindowShouldClose(mainWindow)) {
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vertexArray);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+		process_inputs(mainWindow);
 
-        glfwSwapBuffers(mainWindow);
-        glfwPollEvents();
-    }
+		glClearColor(screenColors[0], screenColors[1], screenColors[2], 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-    glDeleteVertexArrays(1, &vertexArray);
-    glDeleteBuffers(1, &vertexBuffer);
-    glDeleteProgram(shaderProgram);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vertexArray);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
-    glfwTerminate();
-    return 0;
+		glfwSwapBuffers(mainWindow);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &vertexArray);
+	glDeleteBuffers(1, &vertexBuffer);
+	glDeleteProgram(shaderProgram);
+
+	glfwTerminate();
+	return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow *window, const int width, const int height) {
-    glViewport(0, 0, width, height);
-    printf("Framebuffer size: %dx%d\n", width, height);
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height) {
+	glViewport(0, 0, width, height);
+	printf("Framebuffer size: %dx%d\n", width, height);
 }
 
-void process_inputs(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void process_inputs(GLFWwindow* window) {
 
-    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-        screenColors[0] += 0.05f;
-        screenColors[1] -= 0.05f;
-        screenColors[2] += 0.05f;
-    }
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		screenColors[0] += 0.05f;
+		screenColors[1] -= 0.05f;
+		screenColors[2] += 0.05f;
+	}
 }
-
 
 unsigned int build_shader_program() {
-    unsigned int vertexShader = compile_glsl_shader("res/shaders/VertexShader.glsl", GL_VERTEX_SHADER);
-    unsigned int fragmentShader = compile_glsl_shader("res/shaders/FragmentShader.glsl", GL_FRAGMENT_SHADER);
 
-    if (vertexShader == 0 || fragmentShader == 0) {
-        printf("Error compiling shaders, exiting...\n");
-    }
+	unsigned int vertexShader	= compile_glsl_shader("res/shaders/VertexShader.glsl", GL_VERTEX_SHADER);
+	unsigned int fragmentShader = compile_glsl_shader("res/shaders/FragmentShader.glsl", GL_FRAGMENT_SHADER);
 
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+	if (vertexShader == 0 || fragmentShader == 0)
+		printf("Error compiling shaders, exiting...\n");
 
-    int success;
-    char infoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
 
-    if (!success) {
-        glGetShaderInfoLog(shaderProgram, 512, nullptr, infoLog);
-        printf("Error during shader linking: %s", infoLog);
-    }
+	int	 success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+	if (!success) {
+		glGetShaderInfoLog(shaderProgram, 512, nullptr, infoLog);
+		printf("Error during shader linking: %s", infoLog);
+	}
 
-    return shaderProgram;
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
 }
 
-void bind_vao_vbo(unsigned int *vertexBuffer, unsigned int *vertexArray) {
-    float vertices[] = {
-        -0.5f, -0.5f, 0,
-        0.5f, -0.5f, 0,
-        0, 0.5f, 0,
-    };
+void bind_vao_vbo(unsigned int* vertexBuffer, unsigned int* vertexArray) {
 
-    glGenVertexArrays(1, vertexArray);
-    glGenBuffers(1, vertexBuffer);
+	// clang-format off
+	float vertices[] = {
+		-0.5f, -0.5f, 0,
+		 0.5f, -0.5f, 0,
+		    0,  0.5f, 0,
+	};
+	// clang-format on
 
-    glBindVertexArray(*vertexArray);
+	glGenVertexArrays(1, vertexArray);
+	glGenBuffers(1, vertexBuffer);
 
-    glBindBuffer(GL_ARRAY_BUFFER, *vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindVertexArray(*vertexArray);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-    glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, *vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 
-unsigned int compile_glsl_shader(const char *shaderName, const unsigned int shaderType) {
-    char *shaderPtr = readLines(shaderName);
+unsigned int compile_glsl_shader(const char* shaderName, const unsigned int shaderType) {
 
-    const unsigned int vertexShader = glCreateShader(shaderType);
-    glShaderSource(vertexShader, 1, &shaderPtr, nullptr);
-    glCompileShader(vertexShader);
+	char* shaderPtr = readLines(shaderName);
 
-    free(shaderPtr);
+	const unsigned int vertexShader = glCreateShader(shaderType);
+	glShaderSource(vertexShader, 1, &shaderPtr, nullptr);
+	glCompileShader(vertexShader);
 
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	free(shaderPtr);
 
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        printf("Error during shader compilation: %s", infoLog);
-        return 0;
-    }
+	int	 success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
-    return vertexShader;
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		printf("Error during shader compilation: %s", infoLog);
+		return 0;
+	}
+
+	return vertexShader;
 }
